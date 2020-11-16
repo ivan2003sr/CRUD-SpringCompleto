29292fa8184899193417799d2328e226c9c94bc1
ivan2003sr <ivan_2003sr@hotmail.com>
2020-11-16 14:34:35 -0300

    Se importaron las librerías, se creó el controlador y el jsp lista-clientes

---
+++ src/com/ivan2003sr/controlador/Controlador.java
@@ -0,0 +1,16 @@
+package com.ivan2003sr.controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cliente")
+public class Controlador {
+
+	@RequestMapping("/lista")
+	public String listaClientes(Model elModelo) {
+		
+		return "lista-clientes";
+	}
+}
